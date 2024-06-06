60068dd598aad44d63528336437e0d083ccbbcc1
SIC98 <shininchul98@gmail.com>
2024-06-06 21:09:36 +0900

    Format

--- backtesting/test/__init__.py
+++ backtesting/test/__init__.py
@@ -1,27 +1,32 @@
 """Data and utilities for testing."""
+
 import pandas as pd
 
 
 def _read_file(filename):
     from os.path import dirname, join
 
-    return pd.read_csv(join(dirname(__file__), filename),
-                       index_col=0, parse_dates=True, infer_datetime_format=True)
+    return pd.read_csv(
+        join(dirname(__file__), filename),
+        index_col=0,
+        parse_dates=True,
+        infer_datetime_format=True,
+    )
 
 
-GOOG = _read_file('GOOG.csv')
+GOOG = _read_file("GOOG.csv")
 """DataFrame of daily NASDAQ:GOOG (Google/Alphabet) stock price data from 2004 to 2013."""
 
-EURUSD = _read_file('EURUSD.csv')
+EURUSD = _read_file("EURUSD.csv")
 """DataFrame of hourly EUR/USD forex data from April 2017 to February 2018."""
 
-BTCUSDT = _read_file('BTCUSDT.csv')
+BTCUSDT = _read_file("BTCUSDT.csv")
 """DataFrame of minutely spot market binance BTC/USDT data from 2017-08-17 13:00:00 to 2021-08-01 09:00:00."""
 
-BTCUSDT_1H = _read_file('BTCUSDT_1H.csv')
+BTCUSDT_1H = _read_file("BTCUSDT_1H.csv")
 """DataFrame of hourly spot market binance BTC/USDT data from 2017-08-17 13:00:00 to 2021-08-01 09:00:00."""
 
-ETHUSDT = _read_file('ETHUSDT.csv')
+ETHUSDT = _read_file("ETHUSDT.csv")
 """DataFrame of hourly spot market binance BTC/USDT data from 2017-08-17 13:00:00 to 2021-08-01 09:00:00."""
 
 
@@ -52,8 +57,14 @@ def MIN(arr: pd.Series, n: int) -> pd.Series:
 
 
 def IS_LOCAL_MAX(arr: pd.Series, n: int) -> pd.Series:
-    return pd.Series(arr).rooling(2*n+1).max().shift(-n) == pd.Series(arr).rolling(1).max()
+    return (
+        pd.Series(arr).rooling(2 * n + 1).max().shift(-n)
+        == pd.Series(arr).rolling(1).max()
+    )
 
 
 def IS_LOCAL_MIN(arr: pd.Series, n: int) -> pd.Series:
-    return pd.Series(arr).rooling(2*n+1).min().shift(-n) == pd.Series(arr).rolling(1).min()
+    return (
+        pd.Series(arr).rooling(2 * n + 1).min().shift(-n)
+        == pd.Series(arr).rolling(1).min()
+    )
