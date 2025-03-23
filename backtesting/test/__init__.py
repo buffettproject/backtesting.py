d34d6b172ad0b944731efdd4e3ecd7965d6c8959
SIC98 <shininchul98@gmail.com>
2025-03-23 18:53:44 +0900

    Add RSI, MAX, MIN indicator

--- backtesting/test/__init__.py
+++ backtesting/test/__init__.py
@@ -27,3 +27,30 @@ def SMA(arr: pd.Series, n: int) -> pd.Series:
     Returns `n`-period simple moving average of array `arr`.
     """
     return pd.Series(arr).rolling(n).mean()
+
+
+def RSI(array, n):
+    """Relative strength index"""
+    # Approximate; good enough
+    gain = pd.Series(array).diff()
+    loss = gain.copy()
+    gain[gain < 0] = 0
+    loss[loss > 0] = 0
+    rs = gain.ewm(n).mean() / loss.abs().ewm(n).mean()
+    return 100 - 100 / (1 + rs)
+
+
+def MAX(arr: pd.Series, n: int) -> pd.Series:
+    return pd.Series(arr).rolling(n).max()
+
+
+def MIN(arr: pd.Series, n: int) -> pd.Series:
+    return pd.Series(arr).rolling(n).min()
+
+
+def IS_LOCAL_MAX(arr: pd.Series, n: int) -> pd.Series:
+    return pd.Series(arr).rooling(2 * n + 1).max().shift(-n) == pd.Series(arr).rolling(1).max()
+
+
+def IS_LOCAL_MIN(arr: pd.Series, n: int) -> pd.Series:
+    return pd.Series(arr).rooling(2 * n + 1).min().shift(-n) == pd.Series(arr).rolling(1).min()
